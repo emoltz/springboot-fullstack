c2bb4ecd4facb88e68634e6bd70e119332ba55b1
emoltz <es5541@nyu.edu>
2022-12-19 10:18:41 -0500

    init

---
+++ src/test/java/com/esmoltz/fullstackbackend/FullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esmoltz.fullstackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
