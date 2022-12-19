0da088de105f90a7bace09be0d582b9a038bdf3c
emoltz <es5541@nyu.edu>
2022-12-19 18:25:43 -0500

    table view setup

--- fullstack-frontend/src/App.jsx
+++ fullstack-frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 // import './App.css'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./layout/NavBar";
+import Home from "./pages/Home.jsx";
 
 function App() {
 
   return (
     <div className="App">
         <NavBar />
+        <Home />
     </div>
   )
 }
