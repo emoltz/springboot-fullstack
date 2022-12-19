2c43aeb20bf77d3f03fc860881e445e75c4f85ba
emoltz <es5541@nyu.edu>
2022-12-19 16:50:11 -0500

    setup

---
+++ src/main/java/com/esmoltz/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.esmoltz.fullstackbackend.repository;
+
+import com.esmoltz.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
