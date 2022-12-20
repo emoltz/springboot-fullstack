f8819bc9f9cdfbea064edc42ebb3b3a73ae1dda0
emoltz <es5541@nyu.edu>
2022-12-20 12:11:52 -0500

    update user

--- src/main/java/com/esmoltz/fullstackbackend/controller/UserController.java
+++ src/main/java/com/esmoltz/fullstackbackend/controller/UserController.java
@@ -21,14 +21,27 @@ public class UserController {
     }
 
     @GetMapping("/users")
-    List<User> getAllUsers(){
+    List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     @GetMapping("/user/{id}")
-    User getUserById(@PathVariable Long id){
+    User getUserById(@PathVariable Long id) {
         return userRepository.findById(id)
-                .orElseThrow(()-> new UserNotFoundException(id));
+                .orElseThrow(() -> new UserNotFoundException(id));
     }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+
 
 }
